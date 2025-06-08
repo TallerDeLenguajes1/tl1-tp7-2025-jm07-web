0a43bed600dff4710c22a00124161c130180fcbb
Julieta Mariano <julimariano843@gmail.com>
2025-06-08 19:12:47 -0300

    Agrego  la parte 1 y 2.a del ejercicio 2

---
+++ SisPersonal/Empleado.cs
@@ -0,0 +1,65 @@
+namespace EspacioEmpleado
+{
+    public enum Cargos
+    {
+        Auxiliar = 0,
+        Administrativo = 1,
+        Ingeniero = 2,
+        Especialista = 3,
+        nvestigador = 4
+    }
+    public class Empleado
+    {
+        private string Nombre;
+        private string Apellido;
+        private DateTime FechaDeNacimiento;
+        private char EstadoCivil;
+        private DateTime FechaIngresoEnEmpresa;
+        private double SueldoBasico;
+        private Cargos Cargo;
+
+        public Empleado(string nom, string ape, DateTime nac, char estadoCivil, DateTime ingreso, double sueldoBase, Cargos cargo)
+        {
+            Nombre = nom;
+            Apellido = ape;
+            FechaDeNacimiento = nac;
+            EstadoCivil = estadoCivil;
+            FechaIngresoEnEmpresa = ingreso;
+            SueldoBasico = sueldoBase;
+            Cargo = cargo;
+        }
+
+        public int AntiguedadEmpleado()
+        {
+            DateTime Hoy = DateTime.Now;
+            int antiguedad = Hoy.Year - FechaIngresoEnEmpresa.Year;
+            if ((Hoy.Year > FechaIngresoEnEmpresa.Year && Hoy.Month < FechaIngresoEnEmpresa.Month) || (Hoy.Year > FechaIngresoEnEmpresa.Year && Hoy.Month == FechaIngresoEnEmpresa.Month && Hoy.Day < FechaIngresoEnEmpresa.Day))
+            {
+                antiguedad--;
+            }
+            return antiguedad;
+        }
+
+        public int EdadEmpleado()
+        {
+            DateTime hoy = DateTime.Now;
+            int edad = hoy.Year - FechaDeNacimiento.Year;
+            if ((hoy.Year > FechaDeNacimiento.Year && hoy.Month < FechaDeNacimiento.Month) || (hoy.Year > FechaDeNacimiento.Year && hoy.Month == FechaDeNacimiento.Month && hoy.Day < FechaDeNacimiento.Day))
+            {
+                edad--;
+            }
+            return edad;
+        }
+
+        public int AniosFaltantesParaJubilarse()
+        {
+            int anios = 0;
+            int edad = EdadEmpleado();
+            if (edad < 65)
+            {
+                return 65 - edad;
+            }
+            return 0;
+        }
+    }
+}
---
+++ SisPersonal/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EspacioEmpleado;
+
+Empleado trabajador = new Empleado("Juan", "Castro", new DateTime(2004, 7, 31), 's', new DateTime(2024, 8, 24), 700000, Cargos.Ingeniero);
+
+int antiguedad = trabajador.AntiguedadEmpleado();
+Console.WriteLine("Antiguedad: " + antiguedad);
+int edad = trabajador.EdadEmpleado();
+Console.WriteLine("Edad: " + edad);
+int anios = trabajador.AniosFaltantesParaJubilarse();
+Console.WriteLine("Anios faltantes para su jubilacion: " + anios);
+
+
+
