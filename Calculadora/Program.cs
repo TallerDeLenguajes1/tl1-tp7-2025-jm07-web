aae299929e05fea308066befc881b4dd1c74e4b3
Julieta Mariano <julimariano843@gmail.com>
2025-06-08 14:17:15 -0300

    Resuelvo los errores del ejercicio 1

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -25,38 +25,43 @@ while (continuar != 0)
         while (!double.TryParse(Console.ReadLine(), out num))
         {
             Console.WriteLine("Ingrese un número valido.");
-        }    
-    }
+        }
 
-    switch (op)
-    {
-        case 1:
-            calc.Sumar(num);
-            Console.WriteLine("El valor actual de la calculadora es: " + calc.Resultado);
-            break;
-        case 2:
-            calc.Restar(num);
-            Console.WriteLine("El valor actual de la calculadora es: " + calc.Resultado);
-            break;
-        case 3:
-            calc.Multiplicar(num);
-            Console.WriteLine("El valor actual de la calculadora es: " + calc.Resultado);
-            break;
-        case 4:
-            if(num != 0)
-            {
-                calc.Dividir(num);
+        switch (op)
+        {
+            case 1:
+                calc.Sumar(num);
                 Console.WriteLine("El valor actual de la calculadora es: " + calc.Resultado);
-            }
-            break;
-        case 5:
-            calc.Limpiar();
-            Console.WriteLine("El valor actual de la calculadora es: " + calc.Resultado);
-            break;
-        default:
-            Console.WriteLine("Numero de operación no enconontrado.");
+                break;
+            case 2:
+                calc.Restar(num);
+                Console.WriteLine("El valor actual de la calculadora es: " + calc.Resultado);
+                break;
+            case 3:
+                calc.Multiplicar(num);
+                Console.WriteLine("El valor actual de la calculadora es: " + calc.Resultado);
+                break;
+            case 4:
+                if (num != 0)
+                {
+                    calc.Dividir(num);
+                    Console.WriteLine("El valor actual de la calculadora es: " + calc.Resultado);
+                }
+                break;
+            default:
+                Console.WriteLine("Numero de operación no enconontrado.");
+                break;
+        }
+    }
+    else
+    {
+        calc.Limpiar();
+        Console.WriteLine("El valor actual de la calculadora es: " + calc.Resultado);
     }
 
-    Console.WriteLine("Desea continuar? (SI = 1, NO = 0); ");
-    int.TryParse(Console.ReadLine(), out continuar);
+    do
+    {
+        Console.WriteLine("¿Desea continuar? (SI = 1, NO = 0); ");
+        int.TryParse(Console.ReadLine(), out continuar);
+    } while (continuar != 0 && continuar != 1);
 }
